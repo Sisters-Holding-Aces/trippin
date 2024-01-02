09d727d4d09f778a0db3e9604f3588b6c3aefe87
CamVale <cameronvale1@gmail.com>
2024-01-02 11:56:27 +0000

    creates app

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { db } from './firebaseconfig';
+import { collection, getDocs } from 'firebase/firestore';
 
 export default function App() {
+
+
+  const [test, setTest] = useState("")
+  const holidayCollectionRef = collection(db, "TEST");
+
+  useEffect(()=>{
+    const getTest = async () => {
+      try {
+        const data = await getDocs(holidayCollectionRef)
+        
+      setTest(data.docs[0])
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    getTest()
+  }, [])
+
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{test}</Text>
       <StatusBar style="auto" />
     </View>
   );
