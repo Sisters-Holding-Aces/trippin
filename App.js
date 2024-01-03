9fc2ad571a01ce9ab56477328ef2e7d98cfb365c
Ak5cel <akhilaficel@gmail.com>
2024-01-03 08:43:08 +0530

    add mapbox map

--- App.js
+++ App.js
@@ -1,37 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { registerRootComponent } from "expo";
-import { db } from "./firebaseconfig";
-import { collection, getDocs } from "firebase/firestore";
 import Mapbox from "@rnmapbox/maps";
 
 Mapbox.setAccessToken(process.env.MAPBOX_PUBLIC_API_KEY);
 
 export default function App() {
-  const [test, setTest] = useState("");
-  const testRef = collection(db, "test-collection");
-
-  useEffect(() => {
-    const getTest = async () => {
-      try {
-        const contents = await getDocs(testRef);
-
-        const doc = contents.docs[0];
-
-        setTest(doc.data().test_field);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    getTest();
-  }, []);
-
   return (
-    <View style={styles.container}>
-      <Text>mic test from the dev build</Text>
-      <Text>{test}</Text>
-      <StatusBar style="auto" />
+    <View style={styles.page}>
+      <View style={styles.container}>
+        <Mapbox.MapView style={styles.map} />
+      </View>
     </View>
   );
 }
@@ -39,10 +17,16 @@ export default function App() {
 registerRootComponent(App);
 
 const styles = StyleSheet.create({
-  container: {
+  page: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
+    height: 850,
+    width: 500,
+  },
+  map: {
+    flex: 1,
   },
 });
