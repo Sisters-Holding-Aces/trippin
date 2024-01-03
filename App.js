ff7d0b7ddab88bcbc9c1e071f84ebb2bc31ddb25
Andrew-25 <andrewoozs@hotmail.com>
2024-01-03 12:17:32 +0000

    added some folders and backend example

--- App.js
+++ App.js
@@ -3,24 +3,15 @@ import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { db } from "./firebaseconfig";
 import { collection, getDocs } from "firebase/firestore";
+import { testFunc } from "./utils/backend";
 
 export default function App() {
   const [test, setTest] = useState("");
-  const testRef = collection(db, "test-collection");
 
   useEffect(() => {
-    const getTest = async () => {
-      try {
-        const contents = await getDocs(testRef);
-
-        const doc = contents.docs[0];
-
-        setTest(doc.data().test_field);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    getTest();
+    testFunc().then((res) => {
+      setTest(res)
+    })
   }, []);
 
   return (
