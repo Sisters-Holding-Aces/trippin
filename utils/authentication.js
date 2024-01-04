4f102f88cba65ec2bd4cbf34764c5e660f62419a
alexislam0207 <alexislam0207@gmail.com>
2024-01-04 12:21:02 +0000

    add user functions

--- utils/authentication.js
+++ utils/authentication.js
@@ -1,27 +1,58 @@
 import { auth } from "../firebaseconfig";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
-import { getUserByName, postUser } from "./backend";
-
-
+import {
+  createUserWithEmailAndPassword,
+  deleteUser,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { deleteUserDoc, getUserByName, postUser } from "./backend";
 
 export const createAccount = async (email, pass, username) => {
-    
-    const existingUser = await getUserByName(username)
-    if (existingUser.length === 0){
-        try {
-            const newUser = await createUserWithEmailAndPassword(auth, email, pass)
-            await updateProfile(newUser.user, {displayName: username})
-            postUser(username)
-        } catch (err) {
-            console.log(err)
-        }
+  if (!auth.currentUser) {
+    const existingUser = await getUserByName(username);
+    if (existingUser.length === 0) {
+      try {
+        const newUser = await createUserWithEmailAndPassword(auth, email, pass);
+        await updateProfile(newUser.user, { displayName: username });
+        postUser(username, newUser.user.email);
+        return "user created";
+      } catch (err) {
+        return err;
+      }
     } else {
-        console.log('username taken')
+      return "username taken";
     }
-}
+  } else {
+    return "user is logged in";
+  }
+};
 
 export const logIn = async (email, pass) => {
-    await signInWithEmailAndPassword(auth, email, pass)
-}
+  if (!auth.currentUser) {
+    try {
+      await signInWithEmailAndPassword(auth, email, pass);
+      return "user successfully logged in";
+    } catch (err) {
+      return err;
+    }
+  } else {
+    return "user is logged in";
+  }
+};
 
-export const logOut = async () => await signOut(auth)
+export const logOut = async () => await signOut(auth);
+
+export const removeUser = async () => {
+  let user = auth?.currentUser;
+  try {
+    const userID = await getUserByName(auth.currentUser.displayName);
+    await deleteUser(user);
+    await deleteUserDoc(userID[0].id);
+    return "user deleted";
+  } catch (err) {
+    return err;
+  }
+};
--- utils/backend.js
+++ utils/backend.js
@@ -1,48 +1,69 @@
 import { db } from "../firebaseconfig";
-import { collection, getDocs, doc, getDoc, query, where, addDoc } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  doc,
+  getDoc,
+  query,
+  where,
+  addDoc,
+  setDoc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
 
 const testRef = collection(db, "test-collection");
 const usersRef = collection(db, "users");
 
-
 const getFromDB = async (dbLocation) => {
-    try {
-        const {docs} = await getDocs(dbLocation)
-        return docs.map((doc) => ({
-            ...doc.data(),
-            id: doc.id
-        }))
-    } catch (err) {
-        console.error(err);
-    }
-}
-
+  try {
+    const { docs } = await getDocs(dbLocation);
+    return docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 export const testFunc = async () => {
-    try {
-        const contents = await getDocs(testRef);
-        const doc = contents.docs[0];
-        return doc.data().test_field
-    } catch (err) {
-        console.error(err);
-    }
-}
+  try {
+    const contents = await getDocs(testRef);
+    const doc = contents.docs[0];
+    return doc.data().test_field;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-export const getUsers = async () => await getFromDB(usersRef)
+export const getUsers = async () => await getFromDB(usersRef);
 
 export const getUserByName = async (username) => {
-    try {
-        const q = query(usersRef, where('username', '==', username))
-        const {docs} = await getDocs(q)
-    
-        return docs.map((doc) => ({
-            ...doc.data()
-        }))
-    } catch (err){
-        console.log(err)
-    }
-}
+  try {
+    const q = query(usersRef, where("username", "==", username));
+    const { docs } = await getDocs(q);
 
-export const postUser = async (username) => {
-    await addDoc(usersRef, {username: username})
-}
+    return docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const postUser = async (username, email) => {
+  await addDoc(usersRef, { username: username, email: email });
+};
+
+export const patchUser = async (field, input, userId) => {
+  const docRef = doc(db, "users", userId);
+  const data = { [field]: input };
+  await updateDoc(docRef, data);
+};
+
+export const deleteUserDoc = async (userId) => {
+  const docRef = doc(db, "users", userId);
+  await deleteDoc(docRef);
+};
