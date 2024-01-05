9b6c259119ddc62b46fe6ecfdde98341ac2ae24e
alexislam0207 <alexislam0207@gmail.com>
2024-01-05 16:24:57 +0000

    add edit bio

--- components/Profile.js
+++ components/Profile.js
@@ -1,28 +1,75 @@
 import { useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
-import { Avatar, Card, Text } from "react-native-paper";
-import { getUserInfo } from "../utils/backendView";
+import {
+  Avatar,
+  Card,
+  Text,
+  IconButton,
+  TextInput,
+  Button,
+} from "react-native-paper";
+import { editUserInfo, getUserInfo } from "../utils/backendView";
 
 export default function Profile({ user }) {
-  const [userInfo, setUserInfo]= useState({})
+  const [userInfo, setUserInfo] = useState({});
+  const [bioValue, setBioValue] = useState("");
+  const [inputBoxOpen, setInputBoxOpen] = useState(false);
 
-  useEffect(()=>{
-    getUserInfo(user.displayName).then((res)=>{
-        setUserInfo(res)
-    })
-  },[])
+  useEffect(() => {
+    getUserInfo(user.displayName).then((res) => {
+      setUserInfo(res);
+    });
+  }, []);
+
+  useEffect(() => {
+    setBioValue(userInfo.bio);
+  }, [inputBoxOpen]);
+
+  function handleBioSubmit() {
+    editUserInfo(userInfo.id, "bio", bioValue).then(() => {
+      setInputBoxOpen(false);
+      getUserInfo(user.displayName).then((res) => {
+        setUserInfo(res);
+      });
+    });
+  }
 
   return (
     <View style={styles.container}>
-      <Avatar.Text style={styles.avatar} size={150} label={user.displayName} />
-      {userInfo.bio ? (
-        <Card style={styles.bio}>
-          <Card.Content>
-            <Text variant="titleLarge">Bio</Text>
-            <Text variant="bodyMedium">{userInfo.bio}</Text>
-          </Card.Content>
-        </Card>
-      ) : null}
+      <Avatar.Text style={styles.avatar} size={150} label={user.displayName.slice(0,1).toUpperCase()} />
+
+      <Card style={styles.bio}>
+        <Card.Title
+          title="Bio"
+          titleVariant="titleLarge"
+          right={(props) => (
+            <IconButton
+              {...props}
+              icon={inputBoxOpen ? "close" : "lead-pencil"}
+              onPress={() => {
+                !inputBoxOpen ? setInputBoxOpen(true) : setInputBoxOpen(false);
+              }}
+            />
+          )}
+        />
+        <Card.Content>
+          {inputBoxOpen ? (
+            <View>
+              <TextInput
+                value={bioValue}
+                onChangeText={(text) => setBioValue(text)}
+              />
+              <Button icon="check" mode="contained" onPress={handleBioSubmit}>
+                submit
+              </Button>
+            </View>
+          ) : (
+            <Text variant="bodyMedium">
+              {userInfo.bio ? userInfo.bio : null}
+            </Text>
+          )}
+        </Card.Content>
+      </Card>
     </View>
   );
 }
