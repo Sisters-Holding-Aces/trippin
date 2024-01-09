becf04f0b5e33ba244e400e2d5334d37d048e1d1
avgrimshaw <adamvgrimshaw@outlook.com>
2024-01-09 14:47:41 +0000

    remove zoom bar and add compass

--- components/maps/MapWithPopups.js
+++ components/maps/MapWithPopups.js
@@ -20,8 +20,8 @@ const MapWithPopups = ({ holidays, memories }) => {
   const [selectedHoliday, setSelectedHoliday] = useState(null);
   const [selectedMemory, setSelectedMemory] = useState(null);
   const [moreInfo, setMoreInfo] = useState(false);
-  const [bottomSheet, setBottomSheet] = useState()
-  const [sheetData, setSheetData] = useState()
+  const [bottomSheet, setBottomSheet] = useState();
+  const [sheetData, setSheetData] = useState();
 
   const mapView = useRef(null);
   const camera = useRef(null);
@@ -36,13 +36,16 @@ const MapWithPopups = ({ holidays, memories }) => {
     [holidays]
   );
 
-  const memoryFeatureCollection = useMemo(() => memoriesGeoJsonFromData(memories), [memories]);
+  const memoryFeatureCollection = useMemo(
+    () => memoriesGeoJsonFromData(memories),
+    [memories]
+  );
 
   const onPinPress = async (e) => {
     // gets the geojson feature at the pin
     const feature = e.features[0];
     const { popupType, id } = feature.properties;
-    setSheetData(feature.properties)
+    setSheetData(feature.properties);
 
     // centers the selected pin on the screen
     setCoordinates(feature.geometry.coordinates);
@@ -91,15 +94,26 @@ const MapWithPopups = ({ holidays, memories }) => {
     });
   };
 
-
-  useEffect(()=>{
-    setBottomSheet(<ActionSheet sheetData={sheetData} setMoreInfo={setMoreInfo} />)
-  }, [moreInfo])
+  useEffect(() => {
+    setBottomSheet(
+      <ActionSheet sheetData={sheetData} setMoreInfo={setMoreInfo} />
+    );
+  }, [moreInfo]);
 
   return (
     <View style={styles.container}>
-      <Mapbox.MapView style={styles.map} styleURL={Mapbox.StyleURL.TrafficNight} ref={mapView}>
-        <Mapbox.Camera centerCoordinate={coordinates} animationDuration={700} ref={camera} />
+      <Mapbox.MapView
+        style={styles.map}
+        styleURL={Mapbox.StyleURL.TrafficNight}
+        ref={mapView}
+        compassEnabled={true}
+        scaleBarEnabled={false}
+      >
+        <Mapbox.Camera
+          centerCoordinate={coordinates}
+          animationDuration={700}
+          ref={camera}
+        />
 
         <Mapbox.Images images={{ markerHoliday, markerMemory }} />
 
